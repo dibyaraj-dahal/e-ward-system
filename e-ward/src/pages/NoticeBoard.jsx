f9c878dc325cb0f249aa59b052698073250828e1
dibyaraj <dahaldibyaraj46@gmail.com>
2026-06-24 23:12:21 +0545

    added popup feature in NoticeBoard.jsx

--- e-ward/src/pages/NoticeBoard.jsx
+++ e-ward/src/pages/NoticeBoard.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import NoticeCard from "../components/notice/NoticeCard";
 import NoticeFilters from "../components/notice/NoticeFilters";
 import logo from "../assets/nepal-sarkar.png";
@@ -10,6 +11,8 @@ function NoticeBoard() {
       ward: "Ward 5",
       category: "Tax",
       date: "2026-06-21",
+       content:
+      "Tax collection for Ward 5 will begin from July 1. All citizens are requested to bring their citizenship card and previous tax receipts."
     },
     {
       id: 2,
@@ -17,8 +20,11 @@ function NoticeBoard() {
       ward: "Ward 7",
       category: "Registration",
       date: "2026-06-20",
+       content:
+      "A birth registration awareness program will be held at Ward Office 7 on June 30. All parents are encouraged to attend."
     },
   ];
+  const [selectedNotice, setSelectedNotice] = useState(null);
 
   return (
     <div className="min-h-screen bg-slate-100">
@@ -55,12 +61,15 @@ function NoticeBoard() {
 
           <div className="lg:col-span-2 space-y-6">
 
-            {notices.map((notice) => (
-              <NoticeCard
-                key={notice.id}
-                notice={notice}
-              />
-            ))}
+       {notices.map((notice) => (
+  <div
+    key={notice.id}
+    onClick={() => setSelectedNotice(notice)}
+    className="cursor-pointer"
+  >
+    <NoticeCard notice={notice} />
+  </div>
+))}
 
           </div>
 
@@ -88,6 +97,46 @@ function NoticeBoard() {
         </div>
 
       </div>
+      {selectedNotice && (
+  <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+    <div className="bg-white w-[700px] max-w-[90%] rounded-xl shadow-xl p-6">
+
+      <h2 className="text-3xl font-bold text-purple-700 mb-4">
+        {selectedNotice.title}
+      </h2>
+
+      <div className="space-y-2 text-gray-700">
+        <p>
+          <strong>Ward:</strong> {selectedNotice.ward}
+        </p>
+
+        <p>
+          <strong>Category:</strong> {selectedNotice.category}
+        </p>
+
+        <p>
+          <strong>Published:</strong> {selectedNotice.date}
+        </p>
+      </div>
+
+      <hr className="my-4" />
+
+      <p className="leading-relaxed">
+        {selectedNotice.content}
+      </p>
+
+      <div className="flex justify-end mt-6">
+        <button
+          onClick={() => setSelectedNotice(null)}
+          className="bg-purple-600 hover:bg-purple-700 text-white px-5 py-2 rounded-lg"
+        >
+          Close
+        </button>
+      </div>
+
+    </div>
+  </div>
+)}
 
     </div>
   );
